f4a79852c90a41b5854145a2d9694264c346c090
h4x0rMadness <robertmorrislike@gmail.com>
2020-02-23 12:48:27 -0500

    Add files via upload

---
+++ Info.swift
@@ -0,0 +1,21 @@
+//
+//  Info.swift
+//  H4I_Skill_UI
+//
+//  Created by Kefan Zhang on 2/22/20.
+//  Copyright © 2020 Kefan Zhang. All rights reserved.
+//
+
+import UIKit
+
+class Info: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
